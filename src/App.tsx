1f1e6a331f1cdd4bb27ffd5b21dcf81c0a1e1034
liaoke <111@qq.com>
2020-09-06 22:05:34 +0800

    主题色设置，侧边栏配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,65 @@
-import React from 'react';
-import {Layout, Menu, Breadcrumb} from 'antd';
-import {UserOutlined, LaptopOutlined, NotificationOutlined} from '@ant-design/icons';
+import React, {useState} from 'react';
+import {Layout, Menu, Breadcrumb, Drawer, Tooltip} from 'antd';
+import {
+    UserOutlined,
+    LaptopOutlined,
+    NotificationOutlined,
+    SettingOutlined,
+    CloseOutlined,
+    CheckOutlined
+} from '@ant-design/icons';
 import LHeader from './components/Header';
 import PageRouter from './router/page_router';
-import './App.less';
 
 const {SubMenu} = Menu;
 const {Header, Content, Sider} = Layout;
 
 function App() {
+    const [visible, setVisible] = useState(false);
+    const [themeIndex, setThemeIndex] = useState(0);
+
+    const themeList = [
+        {title: '拂晓蓝(默认)', theme: 'theme-daybreak', color: 'rgb(24, 144, 255)'},
+        {title: '薄暮', theme: 'theme-dust', color: 'rgb(245, 34, 45)'},
+        {title: '火山', theme: 'theme-volcano', color: 'rgb(250, 84, 28)'},
+        {title: '日暮', theme: 'theme-sunset', color: 'rgb(250, 173, 20)'},
+        {title: '明青', theme: 'theme-cyan', color: 'rgb(19, 194, 194)'},
+        {title: '极光绿', theme: 'theme-green', color: 'rgb(82, 196, 26)'},
+        {title: '极客蓝', theme: 'theme-geekblue', color: 'rgb(47, 84, 235)'},
+        {title: '酱紫', theme: 'theme-purple', color: 'rgb(114, 46, 209)'},
+    ];
+
+    const themeBlockList = themeList.map((item, index) => {
+        return <Tooltip title={item.title} key={index}>
+            <div className="theme-color-block" style={{backgroundColor: item.color}}
+                 onClick={() => setThemeIndex(index)}>
+                {index === themeIndex ? <CheckOutlined/> : ''}
+            </div>
+        </Tooltip>
+    });
+
+    const toggleSettingDrawer = () => {
+        setVisible(!visible);
+    };
+
     return (
-        <Layout className="App">
+        <Layout className={'App ' + themeList[themeIndex].theme}>
             <Header className="layout-header">
                 <LHeader/>
             </Header>
             <Layout className="layout-body">
                 <Sider width={200} className="layout-sider">
                     <Menu
+                        theme="dark"
                         mode="inline"
                         defaultSelectedKeys={['5']}
                         defaultOpenKeys={['sub1']}
                         style={{height: '100%', borderRight: 0}}
                     >
-                        <SubMenu key="sub1" title="组件样式" icon={<LaptopOutlined />}>
+                        <SubMenu key="sub1" title="组件样式" icon={<LaptopOutlined/>}>
                             <Menu.Item key="5">UI组件</Menu.Item>
                         </SubMenu>
-                        <SubMenu key="sub2" title="系统管理" icon={<UserOutlined />}>
+                        <SubMenu key="sub2" title="系统管理" icon={<UserOutlined/>}>
                             <Menu.Item key="1">用户管理</Menu.Item>
                             <Menu.Item key="2">角色管理</Menu.Item>
                             <Menu.Item key="3">权限管理</Menu.Item>
@@ -46,6 +80,27 @@ function App() {
                     </Content>
                 </Layout>
             </Layout>
+
+            <div className="app-setting-toggle" style={{right: visible ? '300px' : '0'}} onClick={toggleSettingDrawer}>
+                {visible ? <CloseOutlined/> : <SettingOutlined/>}
+            </div>
+
+            <Drawer
+                forceRender={true}
+                width="300px"
+                placement="right"
+                closable={true}
+                maskClosable={false}
+                onClose={toggleSettingDrawer}
+                visible={visible}
+            >
+                <div className="ant-setting-drawer-content">
+                    <h3 className="ant-setting-drawer-title">主题色</h3>
+                    <div className="theme-color-content">
+                        {themeBlockList}
+                    </div>
+                </div>
+            </Drawer>
         </Layout>
     );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import {Space, Avatar} from 'antd';
+import {Space, Avatar, Popover} from 'antd';
 import {SearchOutlined, MessageOutlined} from '@ant-design/icons';
 
 function LHeader() {
     return (
         <Space size="large">
-             <SearchOutlined/>
-             <MessageOutlined />
-             <Avatar size="large" gap={2}>
+            <Popover content="全局搜索">
+                <SearchOutlined/>
+            </Popover>
+            <Popover content="资讯消息">
+                <MessageOutlined/>
+            </Popover>
+            <Avatar size="large" gap={2}>
                 user
             </Avatar>
         </Space>

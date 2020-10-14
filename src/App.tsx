a3b22715732e0459845ee0d9a18d9d7692f6a77e
liaoke <215437475@qq.com>
2020-10-14 13:59:49 +0800

    主题class绑定在body上，防止部分元素不被包裹作用

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {Layout, Menu, Breadcrumb, Drawer, Tooltip} from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Layout, Menu, Breadcrumb, Drawer, Tooltip } from 'antd';
 import {
     UserOutlined,
     LaptopOutlined,
@@ -11,29 +11,29 @@ import LHeader from './components/Header';
 import PageRouter from './router/page_router';
 import './App.less';
 
-const {SubMenu} = Menu;
-const {Header, Content, Sider} = Layout;
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 function App() {
     const [visible, setVisible] = useState(false);
     const [themeIndex, setThemeIndex] = useState(0);
 
     const themeList = [
-        {title: '拂晓蓝(默认)', theme: 'theme-daybreak', color: 'rgb(24, 144, 255)'},
-        {title: '薄暮', theme: 'theme-dust', color: 'rgb(245, 34, 45)'},
-        {title: '火山', theme: 'theme-volcano', color: 'rgb(250, 84, 28)'},
-        {title: '日暮', theme: 'theme-sunset', color: 'rgb(250, 173, 20)'},
-        {title: '明青', theme: 'theme-cyan', color: 'rgb(19, 194, 194)'},
-        {title: '极光绿', theme: 'theme-green', color: 'rgb(82, 196, 26)'},
-        {title: '极客蓝', theme: 'theme-geekblue', color: 'rgb(47, 84, 235)'},
-        {title: '酱紫', theme: 'theme-purple', color: 'rgb(114, 46, 209)'},
+        { title: '拂晓蓝(默认)', theme: 'theme-daybreak', color: 'rgb(24, 144, 255)' },
+        { title: '薄暮', theme: 'theme-dust', color: 'rgb(245, 34, 45)' },
+        { title: '火山', theme: 'theme-volcano', color: 'rgb(250, 84, 28)' },
+        { title: '日暮', theme: 'theme-sunset', color: 'rgb(250, 173, 20)' },
+        { title: '明青', theme: 'theme-cyan', color: 'rgb(19, 194, 194)' },
+        { title: '极光绿', theme: 'theme-green', color: 'rgb(82, 196, 26)' },
+        { title: '极客蓝', theme: 'theme-geekblue', color: 'rgb(47, 84, 235)' },
+        { title: '酱紫', theme: 'theme-purple', color: 'rgb(114, 46, 209)' },
     ];
 
     const themeBlockList = themeList.map((item, index) => {
         return <Tooltip title={item.title} key={index}>
-            <div className="theme-color-block" style={{backgroundColor: item.color}}
-                 onClick={() => setThemeIndex(index)}>
-                {index === themeIndex ? <CheckOutlined/> : ''}
+            <div className="theme-color-block" style={{ backgroundColor: item.color }}
+                onClick={() => setTheme(index)}>
+                {index === themeIndex ? <CheckOutlined /> : ''}
             </div>
         </Tooltip>
     });
@@ -42,10 +42,21 @@ function App() {
         setVisible(!visible);
     };
 
+    const setTheme = (index: number) => {
+        setThemeIndex(index);
+        // 将主题class设置在body上，某些组件是不在项目容器包裹里的，不设置在body上原生变量无效
+        document.body.className = themeList[index].theme;
+    };
+
+
+    useEffect(() => {
+        setTheme(0);
+    });
+
     return (
-        <Layout className={'App ' + themeList[themeIndex].theme}>
+        <Layout className='App'>
             <Header className="layout-header">
-                <LHeader/>
+                <LHeader />
             </Header>
             <Layout className="layout-body">
                 <Sider width={200} className="layout-sider">
@@ -54,12 +65,12 @@ function App() {
                         mode="inline"
                         defaultSelectedKeys={['5']}
                         defaultOpenKeys={['sub1']}
-                        style={{height: '100%', borderRight: 0}}
+                        style={{ height: '100%', borderRight: 0 }}
                     >
-                        <SubMenu key="sub1" title="组件样式" icon={<LaptopOutlined/>}>
+                        <SubMenu key="sub1" title="组件样式" icon={<LaptopOutlined />}>
                             <Menu.Item key="5">UI组件</Menu.Item>
                         </SubMenu>
-                        <SubMenu key="sub2" title="系统管理" icon={<UserOutlined/>}>
+                        <SubMenu key="sub2" title="系统管理" icon={<UserOutlined />}>
                             <Menu.Item key="1">用户管理</Menu.Item>
                             <Menu.Item key="2">角色管理</Menu.Item>
                             <Menu.Item key="3">权限管理</Menu.Item>
@@ -76,13 +87,13 @@ function App() {
                     <Content
                         className="layout-page-container"
                     >
-                        <PageRouter/>
+                        <PageRouter />
                     </Content>
                 </Layout>
             </Layout>
 
-            <div className="app-setting-toggle" style={{right: visible ? '300px' : '0'}} onClick={toggleSettingDrawer}>
-                {visible ? <CloseOutlined/> : <SettingOutlined/>}
+            <div className="app-setting-toggle" style={{ right: visible ? '300px' : '0' }} onClick={toggleSettingDrawer}>
+                {visible ? <CloseOutlined /> : <SettingOutlined />}
             </div>
 
             <Drawer
